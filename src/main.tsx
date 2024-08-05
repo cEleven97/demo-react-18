19cdf1b33b4aa3d978018e5704d95026367d5157
cEleven97 <cong-11@qq.com>
2024-08-06 01:04:36 +0800

    test

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/home/index.tsx'
 
+const router = createHashRouter([
+  { path: '/', Component: Home },
+  { path: 'page', Component: () => <div>Hello world!</div> },
+])
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
